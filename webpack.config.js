e794d7d3384a37ee5a887c0d385838451a6049fb
Aleksandr <stepanetsaleksandr@gmail.com>
2022-01-28 15:01:25 +0200

    start

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,61 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = (env, argv) => {
-    const isProduction = argv.mode === 'production';
-    const config = {
-        entry: './src/index.jsx',
-        output: {
-            filename: 'bundle.js',
+  const isProduction = argv.mode === "production";
+  const config = {
+    entry: "./src/index.jsx",
+    output: {
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /.(js|jsx?)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
         },
-        module: {
-            rules: [
-                {
-                    test: /.jsx?$/,
-                    use: ['babel-loader'],
-                },
-                {
-                    test: /.s?css$/,
-                    use: [
-                        isProduction
-                            ? MiniCssExtractPlugin.loader
-                            : 'style-loader',
-                        'css-loader',
-                        'sass-loader',
-                    ],
-                },
-            ],
+        {
+          test: /.s?css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
         },
-        resolve: {
-            extensions: ['.js', '.jsx'],
-        },
-        plugins: [
-            new webpack.ProgressPlugin(),
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                template: './src/index.html',
-            }),
-        ],
-        devServer: {
-            hot: true,
-        },
-    };
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+    devServer: {
+      historyApiFallback: true,
+      open: true,
+      hot: true,
+      port: 8080,
+    },
+  };
 
-    if (isProduction) {
-        config.plugins.push(
-            new MiniCssExtractPlugin({
-                filename: '[name].css',
-            }),
-        );
-    }
+  if (isProduction) {
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
 
-    return config;
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      })
+    );
+  }
+
+  return config;
 };
